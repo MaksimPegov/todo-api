b02e8c68248db8acfc0d01c6b99a31e197953ad6
Maksim Pegov <maksim.peg@gmail.com>
2022-11-18 14:00:46 +0100

    docker build fix

--- publish-docker.sh
+++ publish-docker.sh
@@ -3,5 +3,5 @@
 USERNAME=maksimpegov
 IMAGE=todo-server
 
-docker build -t $USERNAME/$IMAGE:latest . && \
+docker buildx build --platform linux/amd64 -t $USERNAME/$IMAGE:latest . && \
 docker push $USERNAME/$IMAGE:latest
